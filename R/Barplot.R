d4f6056743fe70ab89186052d42696779062ea15
sshanyiiii <96471351+sshanyiiii@users.noreply.github.com>
2024-05-21 19:46:51 +0800

    Update Barplot.R

--- R/Barplot.R
+++ R/Barplot.R
@@ -1,1 +1,9 @@
-
+ggplot(stat_cell_num_long, aes(x = sample, y = cell_num, fill = state)) +
+  geom_bar(stat="identity", width=0.65, position=position_dodge()) +
+  scale_fill_manual(values = c("#999999", "#ef8a62")) +
+  theme_classic() +
+  labs(x = "",
+       y = "Number of cell",
+       title = "QC") +
+  theme(axis.text.x= element_text(angle = 45, hjust=1, vjust=1)) +
+  theme(aspect.ratio = 1)
